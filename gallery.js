f36011c42010238a381801a3c91019616f5e5fb6
karenwang37@gmail.com <kywang@google.com>
2020-12-30 14:59:52 -0800

    update gallery

--- gallery.js
+++ gallery.js
@@ -6,3 +6,23 @@ function changeBackground() {
 	imgNum = idx + 1;
    document.getElementById("gallery").style.backgroundImage = "url(images/gallery/" + imgNum  + ".jpg)";
 }
+
+function changeColors(images) {
+    var i = 0;
+    function doNext() {
+        if (i >= images.length){
+            i = 0;
+        }
+        document.getElementById("gallery-screen").src = images[i].image;
+        setTimeout(doNext, images[i++].delay);
+    }
+    doNext();
+}
+
+window.onload = function() {
+    changeColors([{image : "images/gallery/1.jpg", delay : 400},
+             {image : "images/gallery/2.jpg", delay : 500},
+             {image : "images/gallery/3.jpg", delay : 700},
+             {image : "images/gallery/4.jpg", delay : 500},
+             {image : "images/gallery/5.jpg", delay : 700}]);
+};
