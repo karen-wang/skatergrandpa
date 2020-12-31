cd43539766243648449068481fff52ea28ba3494
karenwang37@gmail.com <kywang@google.com>
2020-12-30 17:11:30 -0800

    change gallery to onclick only

--- gallery.js
+++ gallery.js
@@ -5,7 +5,7 @@ function preloadImages(images) {
 	}
 }
 
-function changeColors(images) {
+function changeImages(images) {
     var i = 0;
     function doNext() {
         if (i >= images.length){
@@ -17,12 +17,21 @@ function changeColors(images) {
     doNext();
 }
 
-window.onload = function() {
-	var images = [{url : "images/gallery/1.jpg", delay : 400},
+var images = [{url : "images/gallery/1.jpg", delay : 400},
              {url : "images/gallery/2.jpg", delay : 500},
              {url : "images/gallery/3.jpg", delay : 700},
              {url : "images/gallery/4.jpg", delay : 500},
              {url : "images/gallery/5.jpg", delay : 700}];
+window.onload = function() {
 	preloadImages(images);
-    changeColors(images);
+    //changeImages(images);
 };
+
+var i = 0;
+function changeImage() {
+    if (i >= images.length){
+    	i = 0;
+    }
+    document.getElementById("retro-tv").style.backgroundImage = "url(" + images[i].url + ")";
+    i++;
+}
